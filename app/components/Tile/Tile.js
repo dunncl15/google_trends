35b7397e42d43c0cc1f8430bb6cf31d86048d64a
Charles Dunn <dunnc1551@gmail.com>
2019-04-11 22:04:34 -0600

    Phase one of typing animation.

--- app/components/Tile/Tile.js
+++ app/components/Tile/Tile.js
@@ -4,11 +4,49 @@ import './Tile.scss';
 class Tile extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      previousText: '',
+      currentText: '',
+      charIndex: 0,
+      intervalId: null,
+    };
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(() => this.animateText(), 500);
+    this.setState(() => ({ intervalId }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.text && !this.state.text) {
+      clearInterval(this.state.intervalId);
+    }
+  }
+
+  capitilize(text) {
+    const first = text.slice(0, 1).toUpperCase();
+    return first + text.slice(1);
+  }
+
+  animateText() {
+    const animal = this.capitilize(this.props.text);
+    if (this.state.charIndex < animal.length) {
+      this.setState(({ currentText, charIndex }) => ({
+        currentText: (currentText += animal.charAt(charIndex)),
+        charIndex: charIndex + 1,
+      }));
+    } else {
+      // ANIMATION COMPLETE
+      // Clear current interval, reset state, set previousText to currentText
+      // Get new animal name
+      // Get new tile color
+      // Transition tile color
+    }
   }
 
   render() {
-    const { color, width, height, text } = this.props;
+    const { color, width, height } = this.props;
+    const { currentText } = this.state;
     return (
       <div
         className="tile"
@@ -18,7 +56,7 @@ class Tile extends Component {
           width,
         }}
       >
-        {text}
+        <span className="tile-text">{currentText}</span>
       </div>
     );
   }
